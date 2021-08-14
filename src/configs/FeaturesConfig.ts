f28064e63eeb20cf49884ee6d7d15d65f3bb3cdb
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2021-08-14 22:03:15 +0530

    content change

--- src/configs/FeaturesConfig.ts
+++ src/configs/FeaturesConfig.ts
@@ -5,26 +5,26 @@ const FeatureConfig = [
     key: 1,
     title: "Improved customer understanding",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "Focus on acquiring more customers to boost CLV can still lead to higher churn risk (i.e. shorter lifetime) if customer experience is bad. Instead, the aim should be to know what your customers find valuable and deliver the best customer experience.",
     image: Assets.Suburban,
   },
   {
     key: 2,
     title: "Improved Upselling / Cross Selling opportunities​",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "The main benefits of cross-selling include increased sales revenue, improve customer satisfaction and in B2B businesses, increased Customer Lifetime Value (CLV) through deeper integration in a customer's business. When it works, cross-selling is great for both you and for your customers.",
     image: Assets.Divorced,
   },
   {
     key: 3,
     title: "Retention of Potential Valuable Customers",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "Proactive retention also allows you to address risk drivers that are less expensive because customer risk is intercepted when the customer hasn’t canceled. Early detection coupled with micro-segmentation also enables you to A/B test personalized offers for higher net present value (NPV), and thus reduce the cost of retention.",
     image: Assets.Rural,
   },
   {
     key: 4,
-    title: "Avoiding  less Potential customers",
+    title: "Avoiding less Potential customers",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     image: Assets.Urban,
@@ -40,7 +40,7 @@ const FeatureConfig = [
     key: 6,
     title: "Improved revenue and growth",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "By calculating customer CLV, you understand customer satisfaction, journey and profile and margin associated with every customer. You get insights into how much you’re spending on acquiring, serving and retaining each customer and how much revenue you’re generating from them. Once you identify different customer segments based on this, it’s easier to allocate a budget to marketing, sales and retention activities.",
     image: Assets.Others,
   },
 ];
